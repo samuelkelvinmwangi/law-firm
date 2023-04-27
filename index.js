de492510ca60497392e3f156f2ca2838d203d46d
samuel <samuelkelvinmwangi@gmail.com>
2023-04-27 03:50:56 +0300

    adding code to card js

--- index.js
+++ index.js
@@ -16,5 +16,30 @@ function fetchOne(id){
         fee.innerHTML = laws.fee;
     });
 }
-fetchOne(1)
 
+function fetchAllLaws(){
+    fetch(baseUrl)
+    .then((res) => res.json)
+    .then(laws => {
+        const card = `<div class="practice-areas">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card h-100">
+              
+              <div class="card-body">
+                <h5 id="title" class="card-title">${laws.name}</h5>
+                <p id="description"class="card-text">${laws.description}</p>
+                <p class="card-text">Reach out to us <a class="services-link" href="#contact">here</a> for
+                  enquiries or go ahead to book your appointment. </p>
+                <p>Consultation fee:<strong id="fee"class="fee"> ${laws.fee}</strong></p>
+                <a href="" class="btn   mb-3">Book Appointment</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+      container.inserAdjacentHTML("aftebegin", card);
+    })
+}
+fetchAllLaws()
