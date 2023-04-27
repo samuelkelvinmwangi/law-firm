d94a19225f1d8d60d3a758eecf4d117fe0c621e8
samuel <samuelkelvinmwangi@gmail.com>
2023-04-27 12:31:23 +0300

    making ammends to the cards section in js

--- index.js
+++ index.js
@@ -18,3 +18,78 @@ function fetchOne(id){
 }
 fetchOne(1)
 
+
+fetch(baseUrl)
+.then((res) => res.json())
+.then(laws => {
+    laws.forEach((law) => {
+        const card = `<div class="container">
+        <div class="practice-areas">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card h-100">    
+                <div class="card-body">
+                  <h5 id="title" class="card-title">${law.name}</h5>
+                  <p id="description"class="card-text">${law.description}</p>
+                  <p class="card-text">Reach out to us <a class="services-link" href="#contact">here</a> for
+                    enquiries or go ahead to book your appointment. </p>
+                  <p>Consultation fee:<strong id="fee"class="fee"> ${law.fee}</strong></p>
+                  <a href="" class="btn   mb-3">Book Appointment</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+
+      
+    })
+})
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+    // Form submit handler
+    $("#form-info").submit(function(event) {
+      event.preventDefault();
+      var fullName = $("#fullname").val();
+      var timeOfApp = $("#time option:selected").val();
+      var serviceType = $("#service option:selected").val();
+      var communication = $("#comm option:selected").val();
+      var date = new Date($("#get-date").val()).toDateString();
+  
+      // Check if form is filled
+      if (fullName == ""|| date == "" || timeOfApp == "" || serviceType == "" || communication == "") {
+        alert("please fill in your information below")
+      } else {
+        $("#displayAppointment").text("Dear " + fullName + "," + "your appointment is on " + date + "," + " at " + timeOfApp);
+        $("#modeOfComm").text("Your consultation will be held via " + communication);
+        $("#serviceType").text(serviceType);
+      }
+    });
+  
+    // Payment click handler
+    $("#pay").click(function() {
+      $("#payment").slideDown();
+    });
+  
+    // Payment submit handler
+    $("#payment").submit(function(event) {
+      event.preventDefault();
+      var paymentName = $("#pname").val();
+      var email = $("#emailaddress").val();
+  
+      // Check if payment form is filled
+      if (paymentName == " " || email == " ") {
+        alert("Kindly input your payment information.");
+      } else {
+        alert ("Dear " + paymentName + "." + "Thank you for your "  + "transaction.")
+        alert("We will send your receipt and more information on your session to " + email + ".");
+      }
+    });
+  });
